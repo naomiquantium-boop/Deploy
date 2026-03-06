758ea8c67a5682bc5fd4859f5fb3832a1f432ba6
Naomi <naomiquantium@gmail.com>
2026-03-06 10:32:47 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+TELEGRAM_BOT_TOKEN = "8213864135:AAFCqz7JthTGCqlAk8Q8gz7cziaKjVDtq9I"
+TELEGRAM_CHAT_ID = "-1002469576278"  # The channel ID where the bot will post
+TON_NODE_URL = "https://toncenter.com/api/v2/jsonRPC"  # Example public TON node
+TON_API_KEY = "AHMOESTCC42CZHQAAAACPOOEDKBXBONZJTASZN5GI556MV6ILCC3DTCTH74KA4QI4KI6EVQ"  # Optional, depending on the node
